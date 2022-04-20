88c4458882c596f9b5de759e84e76248910c61a0
James Pritchard <james.edward.pritchard@gmail.com>
2022-04-19 21:23:17 -0400

    styling and added content to blog

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,11 @@ import {useState} from 'react';
 
 const Home = () => {
     const [blog, setBlog] = useState([
-        {title: 'My New Website', body: 'lorem ipsum...', author: 'James Pritchard', id:1},
-        {title: 'Welcome Party', body: 'lorem ipsum...', author: 'Bob Smith', id:2},
-        {title: 'Basic React Blog', body: 'lorem ipsum...', author: 'Joanne Micheals', id:3},
-        {title: 'Sample Blog Post', body: 'lorem ipsum...', author: 'Roger Johnson', id:4}
+        {title: 'My New Website', body: 'This is a beginner react project blog', author: 'James Pritchard', id:1},
+        {title: 'Welcome Party', body: 'Getcha party hats on because...', author: 'Bob Smith', id:2},
+        {title: 'Basic React Blog', body: 'Examples of how to use React to...', author: 'Joanne Micheals', id:3},
+        {title: 'Sample Blog Post', body: 'I just got a new puppy. Her name is...', author: 'Roger Johnson', id:4},
+        {title: 'Shrimp Gumbo Recipe', body: 'Good ole New Orleans Shrimp Gumbo...', author: 'Claire Clark', id:5}
     ])
 
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             {blog.map((blog)=>(
                 <div className="blog-preview" key={blog.id}>
                     <h3> {blog.title} </h3>
+                    <p>{blog.body}</p>
                     <small> by {blog.author} </small>
                 </div>
             ))}
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
 		<nav className="navbar">
-			<h1>The Blog</h1>
+			<h1 className="mainTitle">The Blog</h1>
 			<div className="links">
 				<a href="/">Home </a>
 				<a href="/create">New Blog </a>
