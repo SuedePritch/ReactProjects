1ff3454ed074e28bdc87a9fe5a1fe0b88480478f
James Pritchard <james.edward.pritchard@gmail.com>
2022-04-19 21:03:13 -0400

    Basic useState Blog

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
-const Home = () => {
+import {useState} from 'react';
 
-    const handleClick = (e) =>{
-        console.log('i have been clicked', e);
-    }
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e);
-    }
+const Home = () => {
+    const [blog, setBlog] = useState([
+        {title: 'My New Website', body: 'lorem ipsum...', author: 'James Pritchard', id:1},
+        {title: 'Welcome Party', body: 'lorem ipsum...', author: 'Bob Smith', id:2},
+        {title: 'Basic React Blog', body: 'lorem ipsum...', author: 'Joanne Micheals', id:3},
+        {title: 'Sample Blog Post', body: 'lorem ipsum...', author: 'Roger Johnson', id:4}
+    ])
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click ME</button>
-            <button onClick={(e) => handleClickAgain('james', e.target)}>Click Me Again</button>
+            {blog.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h3> {blog.title} </h3>
+                    <small> by {blog.author} </small>
+                </div>
+            ))}
         </div>
     );
 }
